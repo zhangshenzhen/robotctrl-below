174455b43f2c3029840d453c08727d02afbc0c03
kjnijk <eggkjn@foxmail.com>
2016-06-18 06:49:09 +0800

    solve confict(expression)

--- app/src/main/java/com/jly/expression/expression.java
+++ app/src/main/java/com/jly/expression/expression.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import com.brick.robotctrl.UserTimer;
 
 import com.ant.liao.GifView;
 import com.ant.liao.GifView.GifImageType;
@@ -16,25 +17,24 @@ public class expression extends Activity implements OnClickListener {
 
 	private GifView gf1;
 	private Button bt;
-	
+
+	UserTimer userTimer = null;
+
 	private int w ;
-    private int h ;
-    private int width ;
-    private int height;
+	private int h ;
+	private int width ;
+	private int height;
 	private boolean f = true;
 	private int count=0;
-
-	int screenWidth;
-	int screenHeight;
-
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
         
-		screenWidth  = getWindowManager().getDefaultDisplay().getWidth();       // 屏幕宽（像素，如：480px）
-		screenHeight = getWindowManager().getDefaultDisplay().getHeight();      // 屏幕高（像素，如：800p）
+		int screenWidth  = getWindowManager().getDefaultDisplay().getWidth();       // 屏幕宽（像素，如：480px）
+		int screenHeight = getWindowManager().getDefaultDisplay().getHeight();      // 屏幕高（像素，如：800p）
 		Log.e("TAG" + "  getDefaultDisplay", "screenWidth=" + screenWidth + "; screenHeight=" + screenHeight);
-		
-		
+
+		userTimer = new UserTimer();
+
 	    w = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
 	    h = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
 		 
@@ -58,6 +58,7 @@ public class expression extends Activity implements OnClickListener {
 	    
 	
 	public void onClick(View v) {
+		userTimer.clearTimerCount();
 		if(v == gf1)
 		{
 			if(f){
@@ -69,22 +70,38 @@ public class expression extends Activity implements OnClickListener {
 			}
 		}else if(v == bt)
 		{
-			gf1.showCover();
-			synchronized (this) {
-				switch (++count){
-					case 1: gf1.setGifImage(R.drawable.complacent);
-						gf1.setGifImageType(GifImageType.COVER);
-						gf1.showAnimation();
-						break;
-					case 2: gf1.setGifImage(R.drawable.anthomaniac);
-						gf1.setGifImageType(GifImageType.COVER);
-						gf1.showAnimation();
-						break;
-					default : gf1.setGifImage(R.drawable.shy);count=0;
-						gf1.setGifImageType(GifImageType.COVER);
-						gf1.showAnimation();
-						break;
-				}
+			switch (++count){
+				case 1: gf1.setGifImage(R.drawable.complacent);
+					//gf1.setGifImageType(GifImageType.COVER);
+					gf1.showAnimation();
+					break;
+				case 2: gf1.setGifImage(R.drawable.anthomaniac);
+					//gf1.setGifImageType(GifImageType.COVER);
+					gf1.showAnimation();
+					break;
+				case 3: gf1.setGifImage(R.drawable.exciting);
+					//gf1.setGifImageType(GifImageType.COVER);
+					gf1.showAnimation();
+					break;
+				case 4: gf1.setGifImage(R.drawable.sigh);
+					//gf1.setGifImageType(GifImageType.COVER);
+					gf1.showAnimation();
+					break;
+				case 5: gf1.setGifImage(R.drawable.smile);
+					//gf1.setGifImageType(GifImageType.COVER);
+					gf1.showAnimation();
+					break;
+				case 6: gf1.setGifImage(R.drawable.tear);
+					//gf1.setGifImageType(GifImageType.COVER);
+					gf1.showAnimation();
+					break;
+				case 7: gf1.setGifImage(R.drawable.think);
+					//gf1.setGifImageType(GifImageType.COVER);
+					gf1.showAnimation();
+					break;
+				default : gf1.setGifImage(R.drawable.shy);count=0;
+					gf1.showAnimation();
+					break;
 			}
 		}
 	}
