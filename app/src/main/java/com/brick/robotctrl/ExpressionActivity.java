8cc16f530a5b9bba489f67d4adc489c994bb8d04
shenzhen <1624560576@qq.com>
2017-02-07 17:53:25 +0800

    expression funciton add second screen tips

--- app/src/main/java/com/brick/robotctrl/ExpressionActivity.java
+++ app/src/main/java/com/brick/robotctrl/ExpressionActivity.java
@@ -2,20 +2,32 @@ package com.brick.robotctrl;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaRouter;
 import android.os.Bundle;
+import android.os.Debug;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 import com.ant.liao.GifView;
 import com.ant.liao.GifView.GifImageType;
+import com.bumptech.glide.Glide;
+import com.presentation.ExpressionPresentation;
+import com.presentation.SamplePresentation;
+import com.rg2.activity.*;
 
-public class ExpressionActivity extends BaseActivity implements OnClickListener {
+import java.util.Random;
+
+public class ExpressionActivity extends com.rg2.activity.BaseActivity  {
 	private static final String TAG = "ExpressionActivity";
-
+   private ExpressionPresentation   mexpressionPresentation;
 	private static GifView gifView;
 	private int index = 0;
 //	UserTimer userTimer = null;
@@ -23,6 +35,14 @@ public class ExpressionActivity extends BaseActivity implements OnClickListener 
 	private GestureDetector mGestureDetector;
 	private int screenWidth;
 	private int screenHeight;
+
+	// DDMS
+  /*  public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        Debug.stopMethodTracing();
+    }*/
+
+
 	enum EXPRESSION {
 		机器人得意(R.drawable.deyi, "deyi", 0),
 		机器人尴尬(R.drawable.ganga, "ganga", 1),
@@ -78,39 +98,123 @@ public class ExpressionActivity extends BaseActivity implements OnClickListener 
 			return EXPRESSION.机器人可爱;
 		}
 	}
-	public void onCreate(Bundle icicle) {
+
+	/*public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
+		setContentView(R.layout.gif);
+		screenWidth = getWindowManager().getDefaultDisplay().getWidth();       // 屏幕宽（像素，如：480px）
+		screenHeight = getWindowManager().getDefaultDisplay().getHeight();      // 屏幕高（像素，如：800p）
+		Log.e("TAG" + "  getDefaultDisplay", "screenWidth=" + screenWidth + "; screenHeight=" + screenHeight);
+	    Intent intent = getIntent();
+  //   	index = intent.getStringExtra("index");
+ //    Log.e("express","................"+express);
+		index = intent.getIntExtra("index", 6);
+  //	  userTimer = new UserTimer();
+		gifView = (GifView) findViewById(R.id.gif2);
+  // 	gifView.setOnClickListener(this);
+		gifView.setGifImageType(GifImageType.COVER);
+		//屏幕适配;
+//     gifView.setShowDimension(screenWidth, screenHeight);
+		  changeExpression(index);
+		mGestureDetector = new GestureDetector(this, new ExGestureListener());
+	}*/
+	@Override
+	protected void initViews(Bundle savedInstanceState) {
+		setContentView(R.layout.gif);
 
 		screenWidth = getWindowManager().getDefaultDisplay().getWidth();       // 屏幕宽（像素，如：480px）
 		screenHeight = getWindowManager().getDefaultDisplay().getHeight();      // 屏幕高（像素，如：800p）
 		Log.e("TAG" + "  getDefaultDisplay", "screenWidth=" + screenWidth + "; screenHeight=" + screenHeight);
-
-		Intent intent = getIntent();
-//		index = intent.getStringExtra("index");
-		index = intent.getIntExtra("index", 0);
-
-//		userTimer = new UserTimer();
-
-		setContentView(R.layout.gif);
+		    Intent intent = getIntent();
+		//   	index = intent.getStringExtra("index");
+				//    Log.e("express","................"+express);
+		index = intent.getIntExtra("index", 6);
+		//	  userTimer = new UserTimer();
 		gifView = (GifView) findViewById(R.id.gif2);
-//		gifView.setOnClickListener(this);
+		// 	gifView.setOnClickListener(this);
 		gifView.setGifImageType(GifImageType.COVER);
-		//gifView.setShowDimension(screenWidth, screenHeight);
+		//屏幕适配;
+//     gifView.setShowDimension(screenWidth, screenHeight);
 
 		changeExpression(index);
 		mGestureDetector = new GestureDetector(this, new ExGestureListener());
 	}
+	@Override
+	protected void updatePresentation() {
+		// Log.d(TAG, "updatePresentation: ");
+		//得到当前route and its presentation display
+		MediaRouter.RouteInfo route = mMediaRouter.getSelectedRoute(
+				MediaRouter.ROUTE_TYPE_LIVE_VIDEO);
+		Display presentationDisplay =  route  !=  null ? route.getPresentationDisplay() : null;
+		// 注释 : Dismiss the current presentation if the display has changed.
+		if (mexpressionPresentation != null && mexpressionPresentation.getDisplay() !=  presentationDisplay) {
+			mexpressionPresentation.dismiss();
+			mexpressionPresentation = null;
+		}
+		if (mexpressionPresentation == null &&  presentationDisplay != null) {
+			// Initialise a new Presentation for the Display
+			mexpressionPresentation = new ExpressionPresentation(this,  presentationDisplay);
+			//把当前的对象引用赋值给BaseActivity中的引用;
+			  mPresentation  =  mexpressionPresentation  ;
+				// Log.d(TAG, "updatePresentation: this: "+ this.toString());
+			mexpressionPresentation.setOnDismissListener(mOnDismissListener);
+
+			// Try to show the presentation, this might fail if the display has
+			// gone away in the mean time
+			try {
+				mexpressionPresentation.show();
+			} catch (WindowManager.InvalidDisplayException ex) {
+				// Couldn't show presentation - display was already removed
+				// Log.d(TAG, "updatePresentation: failed");
+				mexpressionPresentation = null;
+			}
+		}
+	}
 
 	@Override
+	protected void initData() {
+	}
+
+	@Override
+	protected void initEvent() {
+	}
+
+	@Override
+	protected void initViewData() {
+	}
+
+	int doX,  doY;
+	int movX,  movY;
+	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		if (mGestureDetector.onTouchEvent(event))
-			return true;
+//		if (mGestureDetector.onTouchEvent(event))
+//			return true;
 
 		// 处理手势结束
 		switch (event.getAction() & MotionEvent.ACTION_MASK) {
 			case MotionEvent.ACTION_UP:
 				endGesture();
+				Intent intent = getIntent();
+				Random  random = new Random();
+				int  express =  random.nextInt(21);
+				Log.e("express","................"+express);
+				index = intent.getIntExtra("index", express);
+				changeExpression(index);
+
+				doX =  (int) event.getX();
+				doY = (int) event.getY();
 				break;
+			case MotionEvent.ACTION_MOVE: //手指滑动界面 随机切换表情;
+			     movX = (int) event.getX();
+				 movY = (int) event.getY();
+				int disX = movX- doX;
+				int disY =  movY- doY;
+			    int dis = (int) Math.sqrt(disX*disX+disY*disY);
+				 if (dis >200) {//滑动距离大于200才执行跳转;
+				Log.d(TAG, "onPreferenceClick:进入" + dis);
+				startActivity(new Intent(ExpressionActivity.this, RobotInfoActivity.class));
+				}
+					break;
 		}
 		return super.onTouchEvent(event);
 	}
@@ -159,12 +263,14 @@ public class ExpressionActivity extends BaseActivity implements OnClickListener 
 			gifView.setGifImage(EXPRESSION.getExpression(index).id);
 			gifView.showAnimation();
 			currentIndex = index;
+        	//Glide.with(RobotApplication.getAppContext()).load(EXPRESSION.getExpression(index).id).into(gifView);
+			Log.d(TAG, "changeExpression: ..... " + index);
 		}
 	}
 
 
 	public void onClick(View v) {
-		clearTimerCount();
+		//clearTimerCount();
 
 		int index = currentIndex;
 		index++;
@@ -179,7 +285,6 @@ public class ExpressionActivity extends BaseActivity implements OnClickListener 
 		changeMotionIntent.putExtra("index", index);
 		context.startActivity(changeMotionIntent);
 	}
-
 	// 接收尚未写好，需要再try中判断传递的参数是什么类型
 	public static void startAction(Context context, String expName) {
 		Intent changeMotionIntent = new Intent();
@@ -187,10 +292,21 @@ public class ExpressionActivity extends BaseActivity implements OnClickListener 
 		changeMotionIntent.putExtra("expName", expName);
 		context.startActivity(changeMotionIntent);
 	}
-    @Override
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		updatePresentation();
+	}
+
+	@Override
     protected void onStop() {
         Log.i(TAG, "onStop");
         currentIndex = -1;		// restart expression
         super.onStop();
+		if ( mexpressionPresentation!= null) {
+			mexpressionPresentation.dismiss();
+			mexpressionPresentation = null;
+		}
     }
 }
