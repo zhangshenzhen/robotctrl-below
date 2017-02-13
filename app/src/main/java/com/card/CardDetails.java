4050a45df90e0c5bc10ebd4a76933e712d647bf4
zsz <1624560576@qq.con>
2017-02-13 14:04:32 +0800

    新建Activity试验提交到代码仓库

---
+++ app/src/main/java/com/card/CardDetails.java
@@ -0,0 +1,38 @@
+package com.card;
+
+import android.os.Bundle;
+
+import com.brick.robotctrl.R;
+import com.rg2.activity.BaseActivity;
+
+/**
+ * Created by lx on 2017/2/13.
+ */
+
+public class CardDetails extends BaseActivity {
+
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+           setContentView(R.layout.activity_card_details);
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+    @Override
+    protected void updatePresentation() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initViewData() {
+
+    }
+}
